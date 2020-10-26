26a0b5f81010f8f87bce67d4ef200044bf1cc1e2
seba <sebastian.blazquez96@gmail.com>
2020-10-26 19:08:03 -0300

    Agrego extensión uuid

--- src/infra/migration/1603743336652-CrearPublicacion.ts
+++ src/infra/migration/1603743336652-CrearPublicacion.ts
@@ -4,6 +4,7 @@ export class CrearPublicacion1603743336652 implements MigrationInterface {
     name = 'CrearPublicacion1603743336652'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
         await queryRunner.query(`CREATE TABLE "publicacion" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "titulo" character varying NOT NULL, "descripcion" text NOT NULL, "precioPorNoche" double precision NOT NULL, "cantidadDeHuespedes" integer NOT NULL, "direccionCalle" character varying NOT NULL, "direccionNumero" integer NOT NULL, CONSTRAINT "PK_c14e747a7cc735a880bfbf58a70" PRIMARY KEY ("id"))`);
     }
 
