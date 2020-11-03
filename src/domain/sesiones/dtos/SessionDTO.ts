58d23c5048438d769fb935956ff2661ca5a688a5
seba <sebastian.blazquez96@gmail.com>
2020-11-03 01:50:26 -0300

    Quito payload de la respuesta de autenticacion

--- src/domain/sesiones/dtos/SessionDTO.ts
+++ src/domain/sesiones/dtos/SessionDTO.ts
@@ -23,7 +23,7 @@ export class SessionPayloadDTO {
 export class SessionDTO {
     @IsJWT() public token!: string;
 
-    private readonly PAYLOAD_INDEX = 1;
+    private static readonly PAYLOAD_INDEX = 1;
 
     constructor(token: string) {
         this.token = token;
@@ -32,7 +32,7 @@ export class SessionDTO {
     public getPayload(): SessionPayloadDTO {
         const parts: string[] = this.token.split('.');
 
-        const payloadString: string = Buffer.from(parts[this.PAYLOAD_INDEX], 'base64').toString('ascii');
+        const payloadString: string = Buffer.from(parts[SessionDTO.PAYLOAD_INDEX], 'base64').toString('ascii');
 
         const payload = JSON.parse(payloadString);
 
