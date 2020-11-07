7774fecbb1bd775d533327fa4b4c99901fcb30ef
seba <sebastian.blazquez96@gmail.com>
2020-11-07 00:30:54 -0300

    Agrego mount volume

--- docker/build_production_image.sh
+++ docker/build_production_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run node:12.19.0-alpine3.12 npm install && npm run build
+docker run node:12.19.0-alpine3.12 -v ./:/app npm install && npm run build
 docker build . -f docker/Dockerfile -t bookbnb
