72b789363b4bc07b110200cdb1ec9fb7fff3226b
seba <sebastian.blazquez96@gmail.com>
2020-11-14 20:57:24 -0300

    Deploy

--- docker/build_production_image.sh
+++ docker/build_production_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker run -v $(pwd):/app -w /app node:12.19.0-alpine3.12 npm install && npm run build
-docker build/src . -f docker/Dockerfile -t bookbnb
+docker build . -f docker/Dockerfile -t bookbnb
