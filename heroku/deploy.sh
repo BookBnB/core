7b28b4f1209b8601af6a01a08565a7c4291e7365
seba <sebastian.blazquez96@gmail.com>
2021-02-05 02:38:14 -0300

    Use packaged image in heroku deployment

--- heroku/deploy.sh
+++ heroku/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-./docker/build_production_image.sh
-docker tag bookbnb:latest registry.heroku.com/bookbnb-$BRANCH/web
+docker pull $IMAGE_NAME:$IMAGE_VERSION
+docker tag $IMAGE_NAME:$IMAGE_VERSION registry.heroku.com/bookbnb-$BRANCH/web
 echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com
 docker push registry.heroku.com/bookbnb-$BRANCH/web
 curl https://cli-assets.heroku.com/install.sh | sh  # install heroku
--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker pull $IMAGE_NAME:$IMAGE_VERSION
+kubectl -n $KUBE_NAMESPACE apply -f kubernetes/configmap.yaml
 kubectl -n $KUBE_NAMESPACE set image deployment/bookbnb-core bookbnb-core=$IMAGE_NAME:$IMAGE_VERSION
