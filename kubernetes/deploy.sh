10e76778585b8a0145751be92146c29301cad1b4
seba <sebastian.blazquez96@gmail.com>
2021-02-05 17:17:55 -0300

    Fix image update script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kubectl -n $KUBE_NAMESPACE apply -f kubernetes/configmap.yaml
-kubectl -n $KUBE_NAMESPACE set image deployment/bookbnb-core bookbnb-core=$IMAGE_NAME:$IMAGE_VERSION
+kubectl -n $KUBE_NAMESPACE set image deployment/bookbnb-core core=$IMAGE_NAME:$IMAGE_VERSION
