31651f8815a1cfedb7783428fa0c1012d436286a
seba <sebastian.blazquez96@gmail.com>
2021-02-04 23:20:02 -0300

    Fix image push script

--- kubernetes/push_image.sh
+++ kubernetes/push_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-../docker/build_production_image.sh
+docker/build_production_image.sh
 docker tag bookbnb:latest $IMAGE_NAME:$IMAGE_VERSION
 echo "$REGISTRY_PASSWORD" | docker login $REGISTRY_URL -u $REGISTRY_USERNAME --password-stdin
 docker push $IMAGE_NAME:$IMAGE_VERSION
