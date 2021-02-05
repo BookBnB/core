cd80350f468ee1d0f44464d24c50f45e2ca41e10
seba <sebastian.blazquez96@gmail.com>
2021-02-05 00:17:23 -0300

    Push latest tag

--- kubernetes/push_image.sh
+++ kubernetes/push_image.sh
@@ -3,3 +3,4 @@ docker/build_production_image.sh
 docker tag bookbnb:latest $IMAGE_NAME:$IMAGE_VERSION
 echo "$REGISTRY_PASSWORD" | docker login $REGISTRY_URL -u $REGISTRY_USERNAME --password-stdin
 docker push $IMAGE_NAME:$IMAGE_VERSION
+docker push $IMAGE_NAME:latest
